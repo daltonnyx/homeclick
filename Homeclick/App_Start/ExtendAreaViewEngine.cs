8d69527ebc98454fe488d5d751205cf24227cbc1
Quy Truong <quytruong851@gmail.com>
2017-10-27 21:38:38 -0700

    Add Extended ViewEngine for Area View

---
+++ Homeclick/App_Start/ExtendAreaViewEngine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homeclick.App_Start
+{
+    public class ExtendAreaViewEngine : RazorViewEngine
+    {
+        public void AddViewLocationFormat(string paths)
+        {
+            List<string> existingPaths = new List<string>(ViewLocationFormats);
+            existingPaths.Add(paths);
+
+            this.ViewLocationFormats = existingPaths.ToArray();
+        }
+
+        public void AddPartialViewLocationFormat(string paths)
+        {
+            List<string> existingPaths = new List<string>(PartialViewLocationFormats);
+            existingPaths.Add(paths);
+
+            this.PartialViewLocationFormats = existingPaths.ToArray();
+        }
+    }
+}
