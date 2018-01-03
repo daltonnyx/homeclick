9c737b9b0b10a27e45303950cf251fa093fe487f
Quy Truong <quytruong851@gmail.com>
2018-01-04 01:12:57 +0700

    Thêm Symbol vào layout page

---
+++ Homeclick/Controllers/SymbolsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections;
+using VCMS.Lib.Models;
+using System.Net;
+
+namespace Homeclick.Controllers
+{
+    public class SymbolsController : Controller
+    {
+        
+        public ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Symbols
+        public ActionResult CanvasList(int? type)
+        {
+            IList<Symbol> symbols = (from symbol in db.Symbols
+                                       select symbol).ToList<Symbol>();
+
+            
+            if (type != null)
+            {
+                symbols = (from symbol in symbols
+                            where symbol.ProductType.Id == type
+                            select symbol).ToList<Symbol>();
+            }
+            return PartialView(symbols);
+        }
+    }
+}
